720d3acf5b761185c53956446c7374af8000a626
Dimitrios <dimitriosbroumas@gmai.com>
2024-01-31 17:10:27 -0500

    Added mouse look

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,41 +4,81 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
+    // Rigidbody rb;
 
-    // Start is called before the first frame update
+    // float horizontalInput;
+    // float verticalInput;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     MyInput();
+    //     if(Input.GetKey(KeyCode.Space))
+    //     {
+    //         rb.velocity = new Vector3(0, 5, 0);
+    //     }
+
+    //     if(Input.GetKey(KeyCode.W))
+    //     {
+    //         rb.velocity = new Vector3(0, 0, 5);
+    //     }
+
+    //     if(Input.GetKey(KeyCode.S))
+    //     {
+    //         rb.velocity = new Vector3(0, 0, -5);
+    //     }
+
+    //     if(Input.GetKey(KeyCode.A))
+    //     {
+    //         rb.velocity = new Vector3(-5, 0, 0);
+    //     }
+
+    //     if(Input.GetKey(KeyCode.D))
+    //     {
+    //         rb.velocity = new Vector3(5, 0, 0);
+    //     }
+        
+    // }
+   
+    
+    public float moveSpeed;
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+
+    Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
-        {
-            rb.velocity = new Vector3(0, 5, 0);
-        }
+        MyInput();
+    }
 
-        if(Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = new Vector3(0, 0, 5);
-        }
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
 
-        if(Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = new Vector3(0, 0, -5);
-        }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
 
-        if(Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector3(-5, 0, 0);
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector3(5, 0, 0);
-        }
-        
+     private void MovePlayer()
+    {
+        moveDirection = (orientation.forward * verticalInput) + (orientation.right * horizontalInput);
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }
