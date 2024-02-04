a618e1d3ca73025c93345f2eda40da1adcf4e636
Dimitrios <dimitriosbroumas@gmai.com>
2024-02-03 23:27:51 -0500

    Added pizza that changes on touch

--- Assets/Scripts/Cooking/PizzaController.cs
+++ Assets/Scripts/Cooking/PizzaController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PizzaController : MonoBehaviour
 {
+    [SerializeField]
+    public Material pepperoniPizza;
+    Renderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshRenderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pepperoni")
+        {
+            Debug.Log("making pepperoni");
+            meshRenderer.material = pepperoniPizza;
+        }
     }
 }
