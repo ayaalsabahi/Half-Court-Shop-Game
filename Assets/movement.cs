bd76cfaa00d7d31e9f68957d227df81bbb785e1f
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-02-11 16:12:49 -0500

    added font

---
+++ Assets/movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float amplitude = 1.0f; // The amount of vertical movement
+    public float speed = 1.0f; // The speed of the movement
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * speed) * amplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
